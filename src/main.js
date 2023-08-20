60e86b381529d6df797f79a0a3c3b85168ff606e
Christopher <Christopher>
2023-08-20 08:25:25 -0300

    define base app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import router from './router';
+import store from './store/index';
+import App from './App.vue';
+import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton';
+import BaseBadge from './components/ui/BaseBadge'; 
+import BaseSpinner from './components/ui/BaseSpinner';
+import BaseDialog from './components/ui/BaseDialog';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.component('BaseCard', BaseCard);
+app.component('BaseButton', BaseButton);
+app.component('BaseBadge', BaseBadge);
+app.component('BaseSpinner', BaseSpinner);
+app.component('BaseDialog', BaseDialog);
+
+app.mount('#app');
